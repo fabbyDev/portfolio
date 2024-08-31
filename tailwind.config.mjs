d34996812b87517272e344927424032ecc1ec8b2
Mateo Cruz <mateofabiancruz@gmail.com>
2024-08-30 23:01:11 -0500

    adding custom colors and fontFamily

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,21 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		fontFamily: {
+			sans: ['Inter']
+		},
+		extend: {
+			colors: {
+				primary: {
+					green: 'hsl(150, 68%, 53%)',
+					dark: 'hsl(150, 7%, 11%)',
+				},
+				gray: {
+					17: 'hsla(0, 0%, 75%, 0.17)',
+				},
+				body:  'hsl(180, 1%, 82%)',
+			},
+		},
 	},
 	plugins: [],
 }
