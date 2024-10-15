a0a3d97ece41dff5a16a7b25877de5f6d7d0366c
Mateo Cruz <mateofabiancruz@gmail.com>
2024-10-14 19:42:16 -0500

    deployment config for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://fabbyDev.github.io',
+  base: 'portfolio',
   integrations: [tailwind({
     nesting: true,
     applyBaseStyles: false,
